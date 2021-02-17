ffd24699863798e3e09522bdad81205997d7badb
Mario Lim <mlim1972@gmail.com>
2021-02-17 10:26:38 -0800

    Improve the check to verify mysql is up and running

--- run-mysql.sh
+++ run-mysql.sh
@@ -15,23 +15,18 @@ fi
 
 
 #get mysql server running in port 6603
-docker run -d --name="$MYSQL_CONTAINER_NAME" --env="MYSQL_ROOT_PASSWORD=root" -p 6603:3306 mysql:5.7.29
+docker  run -d --name="$MYSQL_CONTAINER_NAME" --env="MYSQL_ROOT_PASSWORD=root" -p 6603:3306 --health-cmd='mysqladmin ping --silent'  mysql:5.7.29
 
 #wait for mysql server to come up
-while true; do
-  echo "Waiting for MySQL container to come up..."
-  docker exec -i "$MYSQL_CONTAINER_NAME" mysql -h "$HOST_IP" -P 6603 -uroot -proot -e "quit" > /dev/null 2>&1
-  if [ "$?" -eq 0 ];
-  then
-    echo "Connected to MySQL"
-    break
-  else
-    sleep 5s
-  fi
+echo "Waiting for MySQL container to come up..."
+while [ $(docker inspect --format "{{json .State.Health.Status }}" "$MYSQL_CONTAINER_NAME") != "\"healthy\"" ];
+  do printf "."
+  sleep 1
 done
 
 #
 ## Copy the schema to the container
+echo ""
 echo "Copying schema to container"
 docker cp setup-schema.sql "$MYSQL_CONTAINER_NAME":/setup-schema.sql
 
