9d6d19cf45df01df629ae9305f83b4ed937953a1
Mario Lim <mlim1972@gmail.com>
2021-03-08 13:30:37 -0800

    7-OpenAPI branch to show the OpenAPI integration

--- src/main/groovy/com/myexample/controller/ProtectedController.groovy
+++ src/main/groovy/com/myexample/controller/ProtectedController.groovy
@@ -1,12 +1,17 @@
 package com.myexample.controller
 
-import com.myexample.domain.User
 import groovy.util.logging.Slf4j
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 import io.micronaut.security.utils.SecurityService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
 
 @Controller("/protected")
 @Secured(SecurityRule.IS_AUTHENTICATED)
@@ -19,6 +24,18 @@ class ProtectedController {
         this.securityService = securityService
     }
 
+    @SecurityRequirement(name = "BearerAuth")
+    @Secured(SecurityRule.IS_AUTHENTICATED)
+    @Operation(summary = "Greets generic info",
+            description = "A sample protected API"
+    )
+    @ApiResponse(
+            content = @Content(mediaType = "application/json",
+                            schema = @Schema(type="User"))
+    )
+    @ApiResponse(responseCode = "400", description = "Invalid Name Supplied")
+    @ApiResponse(responseCode = "404", description = "Data not found")
+    @Tag(name = "protected")
     @Get
     Map<String, String> index() {
         def authentication = securityService.authentication.get()
