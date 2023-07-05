597c680956232b62d5c66a70c561eeefdd800e41
Mario Lim <mario.lim@openprisetech.com>
2023-07-04 19:19:27 -0700

    user pagination was not correct
    The UserController parameter names was changed to reflect the actual names in the service. Also added test cases to the pagination.

--- src/test/groovy/com/example/controller/UserControllerSpec.groovy
+++ src/test/groovy/com/example/controller/UserControllerSpec.groovy
@@ -103,6 +103,46 @@ class UserControllerSpec extends Specification{
         user.firstName == user2.firstName
     }
 
+    void "test list of user creation and listing via pagination"() {
+        def index = UUID.randomUUID().toString()
+
+        given:
+        def props = [firstName: "${prefix}-${index}.fname".toString(), lastName: "${prefix}-${index}.lname".toString(),
+                        username: "lname-${prefix}-${index}@email.com".toString(), password: "12345"]
+        // create 10 users
+        when:
+        // collect status of all user creation
+        List<Boolean> status = []
+        for (int i = 0; i < 10; i++) {
+            props.firstName = "${prefix}-${index}.fname-${i}".toString()
+            props.lastName = "${prefix}-${index}.lname-${i}".toString()
+            props.username = "uername-${index}.${i}@company.com".toString()
+
+            HttpRequest request = HttpRequest.create(HttpMethod.POST, '/users')
+                    .accept(MediaType.APPLICATION_JSON_TYPE)
+                    .body(props)
+            HttpResponse rsp = client.toBlocking().exchange(request, Argument.of(Map.class), Argument.of(Map.class))
+            status.add(rsp.status == HttpStatus.OK)
+        }
+
+        then:
+        status.size() == 10
+        status.every { it == true }
+
+        when:
+        // call api to get the first page of users
+        HttpRequest request = HttpRequest.create(HttpMethod.GET, "/users?page=1&size=10".toString())
+                .accept(MediaType.APPLICATION_JSON_TYPE)
+        // get the response. The response in the body type is array of users
+        HttpResponse rsp = client.toBlocking().exchange(request, Argument.of(ArrayList.class), Argument.of(Map.class))
+        ArrayList<Map> users = rsp.body()
+
+        then:
+        rsp.status == HttpStatus.OK
+        users.size() == 10
+
+    }
+
     void "test user creation and deletion"() {
         def index = UUID.randomUUID().toString()
 
